97a5159a47345cb3e1fb2b05b44085cba2ed4b0b
voodoo808 <voodoo808@voodoo808.com>
2026-09-03 16:23:38 +0200

    fix: remove erroneous closing tags from BeatsTab
    - BeatsTab was incorrectly closing with </div> and </>
    - These tags belong to Admin, not BeatsTab
    - BeatsTab now correctly ends with just );
    - Fixes 'Unterminated regular expression' build error

--- client/src/pages/Admin.tsx
+++ client/src/pages/Admin.tsx
@@ -2652,8 +2652,6 @@ function BeatsTab({ beats, showForm, setShowForm, editing, setEditing, onRefresh
       {expandedWaveformBeat && (
         <WaveformModal beat={expandedWaveformBeat} onClose={() => setExpandedWaveformBeat(null)} />
       )}
-      </div>
-    </>
   );
 }
 
